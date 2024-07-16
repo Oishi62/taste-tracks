d61266105b3561d2df31e89707d4a1fa6c76b9e2
Oishi Halder <oishihalder8475@gmail.com>
2024-07-16 13:22:27 +0530

    LeftLayout and PostReview Update

--- src/pages/Home_Reviews.jsx
+++ src/pages/Home_Reviews.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import PostReview from '../components/PostReview'
+import LeftLayout from '../components/LeftLayout'
 const Home_Reviews = () => {
   return (
     <>
     <Navbar/>
+    <div className="grid grid-cols-6 gap-2">
+    <div class="col-start-1 col-end-3 ..."><LeftLayout/></div>
+    <div class="col-end-5 col-span-2 ..."><PostReview/></div>
+    
+    </div>
+    
     </>
   )
 }
