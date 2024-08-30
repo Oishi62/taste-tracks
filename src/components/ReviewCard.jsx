1b58c5de892be02b49a819c0805635150b289f79
Oishi Halder <oishihalder8475@gmail.com>
2024-08-30 14:15:07 +0530

    Review Card bg

--- src/components/ReviewCard.jsx
+++ src/components/ReviewCard.jsx
@@ -94,16 +94,24 @@ const ReviewCard = ({docId, reviewIndex,name, location, review, timestamp,initia
       <div className="bg-gray-100 w-full rounded-lg shadow-md flex flex-col transition-all overflow-hidden hover:shadow-2xl">
         
         {/* Top section with background color */}
-        <div className="bg-[url('post_heading.jpeg')] bg-cover bg-no-repeat">
-          <div className="p-4">
-            <div className="pb-3 text-xs font-medium flex justify-between text-blue-900">
-              <span className="flex items-center gap-1">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-                </svg>
-                {formattedDate}
-              </span>
-              <span>
+        <div className="relative">
+  {/* Background Image */}
+  <img 
+    src="post_heading.jpeg" 
+    alt="Background" 
+    className="absolute inset-0 w-full h-full object-cover z-0"
+  />
+
+              <div className="relative z-10">
+                <div className="p-4">
+                  <div className="pb-3 text-xs font-medium flex justify-between text-blue-900">
+                    <span className="flex items-center gap-1">
+                      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+                      </svg>
+                      {formattedDate}
+                    </span>
+                    <span>
               {/* <button onClick={handleLike} className="hover:text-orange-600">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
@@ -112,6 +120,7 @@ const ReviewCard = ({docId, reviewIndex,name, location, review, timestamp,initia
                 {/* <span>{likes}</span>  */}
               </span>
             </div>
+          </div>
           </div>
         </div>
 
